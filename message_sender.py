08d36bd1f8be3922484f7285defeb5866137d802
emocat <97672902+emocat17@users.noreply.github.com>
2025-03-20 13:16:19 +0800

    update

--- message_sender.py
+++ message_sender.py
@@ -1,43 +1,30 @@
-#负责发送消息
+import re
 import requests
+from bs4 import BeautifulSoup
+import json
 import datetime
+import sys
 
-# 设置代理，根据自己实际需求来;这里是因为Qmsg需要，不用Qmsg的话可以删掉；
-#设置代理；用于Qmsg访问
-proxy = "http://127.0.0.1:7890"
-proxies = {
-    "http": proxy,
-    "https": proxy
-}
-# 获取当前时间
 def get_current_time():
     now = datetime.datetime.now()
     return now.strftime("%H:%M:%S")
 
-# 发送QQ消息通知
-def send_qq_message(QmsgKEY):
-    url = f'https://qmsg.zendee.cn/send/{QmsgKEY}'
-    current_time = get_current_time()  # 获取当前时间
-    message = {
+QmsgKEY = "Your_Qmsg_Key"
+url = f'https://qmsg.zendee.cn/send/{QmsgKEY}'
+current_time = get_current_time()  # 获取当前时间
+message = {
         "msg": f"{current_time}  签到成功！",
+       
     }
-    response = requests.post(url, data=message , proxies=proxies)
-    if response.status_code == 200:
-        print("QQ消息发送成功")
-    else:
-        print(f"QQ消息发送失败，状态码: {response.status_code}")
+response = requests.post(url, data=message)
+if response.status_code == 200:
+    print("QQ消息发送成功")
+else:
+    print(f"QQ消息发送失败，状态码: {response.status_code}")
 
-# 发送微信消息通知
-def send_wx_message(WXKey):
-    url = f'https://sctapi.ftqq.com/{WXKey}.send'
-    current_time = get_current_time()  # 获取当前时间
 
-    data = {
-        'text': f"{current_time}  签到成功！",
-        'desp': "签到任务完成"
-    }
-    response = requests.post(url, data=data)
-    if response.status_code == 200:
-        print("WX消息发送成功")
-    else:
-        print(f"WX消息发送失败，状态码: {response.status_code}")
+
+
+
+
+
