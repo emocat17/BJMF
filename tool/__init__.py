cf53f73a770bb3cf80d491e5d83bd452cfd3f3e8
emocat <97672902+emocat17@users.noreply.github.com>
2025-09-12 20:42:23 +0800

    delete

--- tool/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the tool directory a Python package
--- tool/time_utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-# 获取当前时间
-def get_current_time():
-    now = datetime.datetime.now()
-    return now.strftime("%H:%M:%S")
